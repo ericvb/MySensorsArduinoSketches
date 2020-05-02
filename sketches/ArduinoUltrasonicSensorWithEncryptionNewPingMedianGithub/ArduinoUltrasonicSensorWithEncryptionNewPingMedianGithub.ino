8dbaeaa3f85c99f7650e0aab7c02de22b3d118ab
ericvb <ericvb@users.noreply.github.com>
2020-05-02 11:30:01 +0200

    initial upload

---
+++ sketches/ArduinoUltrasonicSensorWithEncryptionNewPingMedianGithub/ArduinoUltrasonicSensorWithEncryptionNewPingMedianGithub.ino
@@ -0,0 +1,107 @@
+/**
+ * The MySensors Arduino library handles the wireless radio link and protocol
+ * between your home built sensors/actuators and HA controller of choice.
+ * The sensors forms a self healing radio network with optional repeaters. Each
+ * repeater and gateway builds a routing tables in EEPROM which keeps track of the
+ * network topology allowing messages to be routed to nodes.
+ *
+ * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
+ * Copyright (C) 2013-2015 Sensnology AB
+ * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
+ *
+ * Documentation: http://www.mysensors.org
+ * Support Forum: http://forum.mysensors.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ *******************************
+ *
+ * REVISION HISTORY
+ * Version 1.0 - Henrik Ekblad
+ * Version 1.1 - Eric Van Bocxlaer - Use of the new ping_median method of the NewPing library
+ * 
+ * DESCRIPTION
+ * This sketch provides an example how to implement a distance sensor using HC-SR04 
+ * http://www.mysensors.org/build/distance
+ */
+
+// Enable debug prints
+//#define MY_DEBUG
+// Enable specific RFM69 debug prints to serial monitor
+//#define MY_DEBUG_VERBOSE_RFM69
+
+#define MY_NODE_ID 1  // set the node ID manually because a MQTT gateway will not assign automatically a node Id - this must be set before the mysensors.h call
+
+// Enable and select radio type attached. Replace the defines if you use other radio type hardware.
+#define MY_RADIO_RFM69
+#define MY_RFM69_FREQUENCY RFM69_868MHZ // Set your frequency here
+#define MY_IS_RFM69HW // Omit if your RFM is not "H"
+#define MY_RFM69_NEW_DRIVER  // soft spi for rfm69 radio works only with new driver
+
+//enable radio communication encryption
+// more information can be found on https://forum.mysensors.org/topic/10382/security-signing-messages-and-encryption-of-messages-a-guide-or-more-a-summary-of-my-tests?_=1588348189475
+//#define MY_ENCRYPTION_SIMPLE_PASSWD "your16bitpassword"
+//enable simple signing
+//#define MY_SIGNING_SIMPLE_PASSWD "your32bitpassword"
+//#define MY_SIGNING_SIMPLE_PASSWD "your16bitpassword"
+//enable simple signing and encryption
+//#define MY_SECURITY_SIMPLE_PASSWORD "your16bitpassword"
+//enable simple signing and encryption
+#define MY_SECURITY_SIMPLE_PASSWORD "your32bitpassword"
+//enable soft signing
+//#define MY_SIGNING_SOFT
+//#define MY_SIGNING_REQUEST_SIGNATURES
+//#define MY_SIGNING_SOFT_RANDOMSEED_PIN A0 
+// following hex codes are dummy hex codes, replace by your hexcodes (see the link above how to generate)
+//#define MY_SIGNING_NODE_WHITELISTING {{.nodeId = 0,.serial = {0x99,0x88,0x77,0x66,0x55,0x44,0x33,0x22,0x11}},{.nodeId = 1,.serial = {0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x88,0x99}}}
+
+#include <SPI.h>
+#include <MySensors.h> // sketch tested with version 2.3.2, see http://librarymanager#MySensors
+#include <NewPing.h>  // sketch tested with version 1.9.1, see http://librarymanager#NewPing
+
+#define SENSOR_NAME "Distance Sensor"
+#define SENSOR_VERSION "1.1"
+
+#define CHILD_ID 1  // Each radio node can report data for up to 254 different child sensors. You are free to choose the child id yourself. 
+                    // You should avoid using child-id 255 because it is used for things like sending in battery level and other (protocol internal) node specific information.
+#define TRIGGER_PIN  6  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     5  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 300 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+unsigned long SLEEP_TIME = 30000; // Sleep time between reads (in milliseconds)
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+MyMessage msg(CHILD_ID, V_DISTANCE);
+int lastDist;
+bool metric = true;
+
+void setup()  
+{ 
+  metric = getControllerConfig().isMetric;
+}
+
+void presentation() {
+  // Send the sketch version information to the gateway and Controller
+  sendSketchInfo(SENSOR_NAME, SENSOR_VERSION);
+
+  // Register all sensors to gw (they will be created as child devices) by their ID and S_TYPE
+  present(CHILD_ID, S_DISTANCE);
+}
+
+void loop()      
+{     
+  // use the build-in digital filter to discard out of range pings
+  int echoTime = sonar.ping_median(10);
+  int dist = metric?sonar.convert_cm(echoTime):sonar.convert_in(echoTime);
+  Serial.print("Ping: ");
+  Serial.print(dist);
+  Serial.println(metric?" cm":" in");
+
+  if (dist != lastDist) {
+      send(msg.set(dist));
+      lastDist = dist;
+  }
+
+  sleep(SLEEP_TIME);
+}
